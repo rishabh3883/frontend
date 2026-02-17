6dcb6a4c9649d42d94a65fe9b2f70eba533bc6ae
Rishabh Gupta <146321370+rishabh3883@users.noreply.github.com>
2026-02-17 08:47:20 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL, {
+const BASE_URL = import.meta.env.VITE_API_URL.replace('/api', '');
+
+const socket = io(BASE_URL, {
   transports: ['websocket'],
+  withCredentials: true,
   autoConnect: true,
 });
 
