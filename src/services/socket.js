2a9a5cd460e4b35568efa70a4a858bf85d8c1a9a
Rishabh Gupta <146321370+rishabh3883@users.noreply.github.com>
2026-02-17 08:40:49 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Backend URL
+const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ['websocket'],
+  autoConnect: true,
+});
 
 export default socket;
