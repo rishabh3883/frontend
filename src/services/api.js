0659fbc5b6fa8af4d3a1122ae46f2e131be59f42
Rishabh Gupta <146321370+rishabh3883@users.noreply.github.com>
2026-02-17 08:24:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://backend-production-eae7.up.railway.app/api',
 });
 
 // Add token to every request
